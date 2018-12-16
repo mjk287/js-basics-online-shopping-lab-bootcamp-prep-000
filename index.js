b1411316706928248cbd033b72fee7bf1db6f696
Myung Kim <mjk287@cornell.edu>
2018-12-16 16:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  if (cardNumber === null){
+    return "Sorry, we don't have a credit card on file for you."
+  }else{
   var totalCost = total()
   cart = []
   return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
