bdc7fcb4da151fa387639b8ba7cb480ff99f9334
Myung Kim <mjk287@cornell.edu>
2018-12-16 15:31:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var cartItem = {item : Math.floor(100)}
+ var cartItem = Object.assign({}, {[item] : Math.floor(100)})
  return cart.push(cartItem)
 }
 
