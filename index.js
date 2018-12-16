ca4a8ac9c66196cb51639fa3ec532bebb37a3557
Myung Kim <mjk287@cornell.edu>
2018-12-16 16:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i, 1)
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
